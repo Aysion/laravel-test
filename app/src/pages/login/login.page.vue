ef0e3b3cb18d77e286a4987d583cf72aabe15a3b
weslen.silva <weslen.silva@gigapixel.com.br>
2021-01-15 08:11:06 -0300

    Install Quasar

---
+++ app/src/pages/login/login.page.vue
@@ -0,0 +1,24 @@
+<template>
+	<q-layout>
+
+		<q-page-container>
+			<q-page>
+				<q-form ref="formLogin">
+					<q-input type="email" name="email" placeholder="E-mail" />
+					<q-input type="password" name="password" placeholder="Senha" />
+				</q-form>
+			</q-page>
+		</q-page-container>
+
+	</q-layout>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
